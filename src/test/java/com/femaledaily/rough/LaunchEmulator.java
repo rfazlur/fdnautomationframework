5bdb3796d2aceeac6dbbf1a1bb6474e62e8d7ebf
Faiz <fazlur.faiz.rahman@gmail.com>
2019-05-08 11:01:11 +0700

    launchemulatorprogrammatically

---
+++ src/test/java/com/femaledaily/rough/LaunchEmulator.java
@@ -0,0 +1,13 @@
+package com.femaledaily.rough;
+
+import java.io.File;
+
+public class LaunchEmulator {
+
+    public static void main(String[] args){
+        String sdkPath = "/home/faiz/Android/Sdk";
+        String emulatorPath = sdkPath + "emulator" + File.separator + "emulator";
+
+    }
+
+}
